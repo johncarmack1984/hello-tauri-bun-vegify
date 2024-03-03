b027882397397617a694f2672ef1514c5c8bf6fa
John Carmack <johncarmack@me.com>
2024-03-02 19:18:26 -0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Greet from "./lib/Greet.svelte";
+</script>
+
+<main class="container">
+  <h1>Welcome to Vegify!</h1>
+
+  <div class="row">
+    <a href="https://vegify.app" target="_blank">
+      <img src="/vegify.svg" class="logo vegify" alt="Vegify Logo" />
+    </a>
+  </div>
+
+  <p>What's your name?</p>
+
+  <div class="row">
+    <Greet />
+  </div>
+</main>
+
+<style>
+</style>
